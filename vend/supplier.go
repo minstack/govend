ec5e154dee266d327c7fd509e040ea8f9744f182
Jack Harrison-Sherlock <jack@harrisonsherlock.com>
2018-06-11 19:26:44 +1200

    export suppliers

--- vend/supplier.go
+++ vend/supplier.go
@@ -1,43 +1,105 @@
 // Package vend handles interactions with the Vend API.
 package vend
 
-// Supplier contains supplier data.
-type SupplierBase struct {
-	Name        string   `json:"name"`
-	Description *string  `json:"description"`
-	Source      *string  `json:"source"`
-	Contact     *Contact `json:"contact"`
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type SupplierCollectionResponse struct {
+	Suppliers  []SupplierBase `json:"suppliers"`
+	Pagination Pagination     `json:"pagination"`
 }
 
 // Supplier contains supplier data.
-type Supplier struct {
-	ID *string `json:"id"`
-	SupplierBase
+type SupplierBase struct {
+	ID          *string  `json:"id,omitempty"`
+	Name        *string  `json:"name,omitempty"`
+	Description *string  `json:"description,omitempty"`
+	Source      *string  `json:"source,omitempty"`
+	Contact     *Contact `json:"contact,omitempty"`
 }
 
 // Contact is a supplier object
 type Contact struct {
-	FirstName         *string `json:"first_name"`
-	LastName          *string `json:"last_name"`
-	CompanyName       *string `json:"company_name"`
-	Phone             *string `json:"phone"`
-	Mobile            *string `json:"mobile"`
-	Fax               *string `json:"fax"`
-	Email             *string `json:"email"`
-	Twitter           *string `json:"twitter"`
-	Website           *string `json:"website"`
-	PhysicalAddress1  *string `json:"physical_address1"`
-	PhysicalAddress2  *string `json:"physical_address2"`
-	PhysicalSuburb    *string `json:"physical_suburb"`
-	PhysicalCity      *string `json:"physical_city"`
-	PhysicalPostcode  *string `json:"physical_postcode"`
-	PhysicalState     *string `json:"physical_state"`
-	PhysicalCountryID *string `json:"physical_country_id"`
-	PostalAddress1    *string `json:"postal_address1"`
-	PostalAddress2    *string `json:"postal_address2"`
-	PostalSuburb      *string `json:"postal_suburb"`
-	PostalCity        *string `json:"postal_city"`
-	PostalPostcode    *string `json:"postal_postcode"`
-	PostalState       *string `json:"postal_state"`
-	PostalCountryID   *string `json:"postal_country_id"`
+	FirstName         *string `json:"first_name,omitempty"`
+	LastName          *string `json:"last_name,omitempty"`
+	CompanyName       *string `json:"company_name,omitempty"`
+	Phone             *string `json:"phone,omitempty"`
+	Mobile            *string `json:"mobile,omitempty"`
+	Fax               *string `json:"fax,omitempty"`
+	Email             *string `json:"email,omitempty"`
+	Twitter           *string `json:"twitter,omitempty"`
+	Website           *string `json:"website,omitempty"`
+	PhysicalAddress1  *string `json:"physical_address1,omitempty"`
+	PhysicalAddress2  *string `json:"physical_address2,omitempty"`
+	PhysicalSuburb    *string `json:"physical_suburb,omitempty"`
+	PhysicalCity      *string `json:"physical_city,omitempty"`
+	PhysicalPostcode  *string `json:"physical_postcode,omitempty"`
+	PhysicalState     *string `json:"physical_state,omitempty"`
+	PhysicalCountryID *string `json:"physical_country_id,omitempty"`
+	PostalAddress1    *string `json:"postal_address1,omitempty"`
+	PostalAddress2    *string `json:"postal_address2,omitempty"`
+	PostalSuburb      *string `json:"postal_suburb,omitempty"`
+	PostalCity        *string `json:"postal_city,omitempty"`
+	PostalPostcode    *string `json:"postal_postcode,omitempty"`
+	PostalState       *string `json:"postal_state,omitempty"`
+	PostalCountryID   *string `json:"postal_country_id,omitempty"`
+}
+
+// Suppliers gets all Suppliers from a store.
+func (c Client) Suppliers() ([]SupplierBase, error) {
+
+	suppliers := []SupplierBase{}
+
+	data, more, p, err := c.Pages("api/supplier", 0)
+
+	suppliers = append(suppliers, data...)
+
+	for more {
+
+		// Continue grabbing pages until we receive an empty one.
+		data, more, p, err = c.Pages("api/supplier", p)
+		if err != nil {
+			return nil, err
+		}
+		// Append supplier page to list of suppliers.
+		suppliers = append(suppliers, data...)
+	}
+
+	return suppliers, err
+}
+
+func (c Client) Pages(resource string, page int64) ([]SupplierBase, bool, int64, error) {
+
+	url := ""
+
+	if page > 0 {
+		url = fmt.Sprintf("https://%s.vendhq.com/%s?page_size=200&page=%v", c.DomainPrefix, resource, page)
+	} else {
+		url = fmt.Sprintf("https://%s.vendhq.com/%s?page_size=200", c.DomainPrefix, resource)
+	}
+
+	body, err := c.MakeRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Error getting resource: %s", err)
+	}
+
+	// Decode the raw JSON.
+	response := SupplierCollectionResponse{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Printf("\nError unmarshalling payload: %s", err)
+		return nil, false, 0, err
+	}
+
+	// Data is the resource body.
+	data := response.Suppliers
+
+	// Page contains the maximum version number of the resources.
+	pages := response.Pagination.Pages
+	pg := response.Pagination.Page
+	more := pg != pages
+
+	return data, more, pg, err
 }
